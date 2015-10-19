b804852752a00d6bc64134f0b386dac325e07f66
Andrew Skaper <andrew@skaper.info>
2015-10-18 19:43:14 -0700

    Added Puppy Love project

---
+++ twitter/js/main.js
@@ -0,0 +1,34 @@
+// As a user, I should be able to
+//
+// Send a new tweet, which should get added to the list of tweets
+// Click any top-level tweet to expand it
+// See all replies to a tweet while it is expanded
+// Reply to any top-level tweet
+// Close/collapse an expanded top-level tweet by clicking it a second time
+
+
+
+$(document(){
+
+
+    var user = {
+        handle: '@bradwestfall',
+        img: 'brad.png'
+    }
+
+
+
+    var template = Handlebars.compile( $('#template-tweet'.html())
+
+
+
+
+
+
+
+
+
+
+
+
+});
