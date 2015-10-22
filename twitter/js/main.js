cda2028b4e19aa13afa72fe5581b175be3cb99dd
Andrew Skaper <andrew@skaper.info>
2015-10-22 10:57:07 -0700

    Changes 102115

--- twitter/js/main.js
+++ twitter/js/main.js
@@ -8,27 +8,63 @@
 
 
 
-$(document(){
+//i need to see what the markup of a new tweet looks like, and then create the appropriate template for it
+
+//i need to create the set of instructions necessary in order to write the markup to the DOM.
+
+$(function(){
 
 
     var user = {
         handle: '@bradwestfall',
-        img: 'brad.png'
+        img: 'images/brad.png'
     }
 
 
 
-    var template = Handlebars.compile( $('#template-tweet'.html())
+    var composeTemplate = Handlebars.compile($('#template-compose').html())
+
+        function renderCompose(){
+            return composeTemplate()
+
+    }
 
 
+    var threadTemplate = Handlebars.compile($('#template-thread').html())
 
+        function renderThread(){
+            return renderThread()
+            
+    }
 
+    var tweetTemplate = Handlebars.compile($('#template-tweet').html())
 
+    function renderTweet(user, message){
+        return tweetTemplate({
+            userImage: user.img,
+            userHandle: user.handle,
+            userMessage: message
+        })
+    }
 
+    $('main').on('submit', '.compose', function(){
+        var message = $(this).find('textarea').val()
 
+        if ($(this).parents('header').length) {
+            console.log('Were making a thread')
+        } else {
+            $(this).parents('.replies').append(renderTweet(user, message))
+        }
+        return false;
+    })
 
+    $('main').on('click', '.compose', function(){
+        $(this).addClass('expand')
 
+    })
 
+    $('main').on('click', '.thread', function(){
+        $(this).addClass('expand')
 
-
+    })
 });
