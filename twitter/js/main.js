f631f0bc8206a43b7d7ef4a6cd06353e51271a38
Andrew Skaper <andrew@skaper.info>
2015-10-26 02:12:17 -0700

    TWitter is DONE

--- twitter/js/main.js
+++ twitter/js/main.js
@@ -1,17 +1,3 @@
-// As a user, I should be able to
-//
-// Send a new tweet, which should get added to the list of tweets
-// Click any top-level tweet to expand it
-// See all replies to a tweet while it is expanded
-// Reply to any top-level tweet
-// Close/collapse an expanded top-level tweet by clicking it a second time
-
-
-
-//i need to see what the markup of a new tweet looks like, and then create the appropriate template for it
-
-//i need to create the set of instructions necessary in order to write the markup to the DOM.
-
 $(function(){
 
     var user = {
@@ -19,81 +5,53 @@ $(function(){
         img: 'images/brad.png'
     }
 
-    var composeTemplate = Handlebars.compile($('#template-compose').html())
+    var makeForm = function renderCompose() {
+		var source   = $("#template-compose").html();
+		var template = Handlebars.compile(source);
+		var context = {};
+		var html    = template(context);
+		return html;
+	}
 
-        function renderCompose(){
-            return composeTemplate({
+    var makeThread = function renderThread(user, message, image) {
+		var source   = $("#template-thread").html();
+		var template = Handlebars.compile(source);
+		var context = {
+			'tweet': makeTweet(user, message, image),
+			'compose': makeForm()
+		};
+		var thread = template(context);
+		return thread;
+	}
 
-            })
-    }
+    var makeTweet = function renderTweet(user, message, image) {
+		var source   = $("#template-tweet").html();
+		var template = Handlebars.compile(source);
+		var context = {User: user, message: message, image: image};
+		var tweet    = template(context);
+		return tweet;
+	}
 
-    // //	var makeForm = function renderCompose() {
-	// 	var source   = $("#template-compose").html();
-	// 	var template = Handlebars.compile(source);
-	// 	var context = {};
-	// 	var html    = template(context);
-	// 	return html;
-	// }
+    $(document).on('submit', 'form', function(event){
+		event.preventDefault();
+		var message = $(this).find('textarea').val();
+		if($(this).is('header > form')) {
+			$(".tweets").prepend(makeThread(user.handle, message, user.img));
+			$(this).find('textarea').val('');
+		} else {
+			$(this).parents('.replies').append(makeTweet(user.handle, message, user.img));
+			$(this).find('textarea').val('');
+		}
+	});
 
-    var threadTemplate = Handlebars.compile($('#template-thread').html())
-        function renderThread(user, message){
-            renderThread({
-            renderTweet: tweetTemplate(user, message),
-            'compose': makeform()
-        })
-        var thread = threadTemplate(renderThread)
-        return thread
-    }
-
-    var tweetTemplate = Handlebars.compile($('#template-tweet').html())
-
-    function renderTweet(user, message){
-        return tweetTemplate({
-            userImage: user.img,
-            userHandle: user.handle,
-            userMessage: message
-        })
-    }
-
-    $('main').on('submit', '.compose', function(){
-        event.preventDefault();
-        var message = $(this).find('textarea').val()
-        console.log(message)
-
-
-        if ($(this).is('header > .compose')) {
-            $('.tweets').append(renderTweet(user, message)).addClass('thread')
-            $('textarea').val('')
-        } else {
-            $(this).append(renderTweet(user, message))
-        }
-        return false
-
-//going to parent, selecting child of thread and appending to that.
-
-        // if ($(this).parents('header').length) {
-        //     console.log('Were making a thread')
-        // } else {
-        //     $(this).parents('.replies').append(renderTweet(user, message))
-        // }
-        // return false;
-    })
-
-    $('main').on('click', '.compose', function(){
-        $(this).toggleClass('expand')
+    $(document).on('click', 'textarea', function(){
+        $(this).parents('.compose').toggleClass('expand')
 
     })
 
-    $('main').on('click', '.tweet', function(){
+    $(document).on('click', '.tweet', function(){
         $(this).parents('.thread').toggleClass('expand')
 
     })
 
-    // $('main').on('click', '.tweet', function(){
-    //     $(this).parents('.thread').toggleClass('expand')
-    // })
-//I WILL GET YOU RIGHT BEFORE I LEAVE GODDAMNIT!!!11one
-    // $(this).on('click', '.thread', function(){
-    //     $('section ').toggleClass('expand')
-    // })
 });
